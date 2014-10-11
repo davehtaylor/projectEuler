3ceca7dba1c94a1d34027dc216326b57d5d25437
Dave Taylor <davehtaylor@me.com>
2014-10-10 22:59:20 -0400

    Completed Problem 4. Instead of just looking for palindromes in the specified range, this iteration multiplies all three digit numbers, and then looks for palindromes. This produces the correct answer.

--- projectEuler004.py
+++ projectEuler004.py
@@ -4,16 +4,21 @@
 #
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
-palindromes = []                    # List to hold all of the palindromes
+palindromes = []                                   # List to hold all of the palindromes
+num1 = 100                                         # Initialize variables two three-digit multiply so we can find the palindromes
+num2 = 100
 
-def reverse(i):                     # A procedure to reverse a number 
-    i = str(i)                      # First convert the number to a string
-    revstring = i[::-1]             # Then reverse it
+def reverse(i):                                    # A procedure to reverse a number 
+    i = str(i)                                     # First convert the number to a string
+    revstring = i[::-1]                            # Then reverse it
     return revstring
 
-for i in xrange (10000,998001):     # Start from lowest number that is the product of two 3-digit numbers: 100 x 100, and go to largest: 999 x 999
-    if str(i) == reverse(i):        # Check if the number is the same backward and forward, i.e., a palindrome
-        palindromes.append(i)       # If it is a palindrome, add it to the list
-    i += 1
+for num1 in xrange (100,1000):                     # For num1, go from 100 to 1000
+    for num2 in xrange (100,1000):                 # For num2, go from 100 to 1000
+        product = num1 * num2                      # Keep multiplying num1 and num2 all the way up to 1000
+        if str(product) == reverse(product):       # If the product is a palindrome, add it to the palindromes[] list
+            palindromes.append(product)
 
-print palindromes[-1]               # Print out the largest palindrome. Currently, this is producing the incorrect answer. 
+palindromes.sort()                                 # Sort the palindromes[] list
+
+print palindromes[-1]                              # Print out the largest palindrome.
