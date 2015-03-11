b9b66109662366091aef13b6e9bdc62a642b851a
Dave Taylor <davehtaylor@me.com>
2015-03-11 01:05:51 -0400

    Problem 4 complete

--- c/projectEuler004.c
+++ c/projectEuler004.c
@@ -3,10 +3,12 @@
 #include <stdio.h>
 
 int reverseNum(int);
+int palindrome(void);
 
 int main(void)
 {
-
+    int answer = palindrome();
+    printf("%d\n", answer);
     return 0;
 }
 
@@ -23,4 +25,33 @@ int reverseNum(int inputNum)
     }
 
     return reverse;
+}
+
+/* Find the largest palindrome */
+int palindrome(void)
+{
+    int num1;
+    int num2;
+    int product;
+    int palindrome;
+
+    for (num1 = 100; num1 < 1000; num1++)
+    {
+        for (num2 = 100; num2 < 1000; num2++)
+        {
+            product = num1 * num2;
+
+            /* Check if the product is a palindrome and also if it is 
+             * larger than the last palindrome in the loop. */
+            if (product == reverseNum(product) && product > palindrome)
+            {
+                palindrome = product;
+            }     
+
+        }
+       
+    }
+
+    return palindrome;
+
 }
