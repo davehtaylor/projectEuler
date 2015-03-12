ca06d0296d34c38871bdeedb4070d34333c5194d
Dave Taylor <dave@localhost.localdomain>
2015-03-12 14:29:31 -0400

    Problem 7 completed. It's a little slower than I would like, though I don't know if that is inherrent given the problem here, or if there is a way to make it more efficient. Given Project Euler's seeming desire for elegance, I tend to favor the latter. However, it does produce the correct answer in ~ 10 seconds or so.

---
+++ c/projectEuler007.c
@@ -0,0 +1,68 @@
+/* What is the 10,001st prime number? */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int lowest(int);
+bool isPrime(int);
+int calculatePrimes(void);
+
+int main(void)
+{
+    int answer = calculatePrimes(); 
+    printf("%d\n", answer);
+    return 0;
+}
+
+/* Find the lowest whole number the input number is divisible by */
+int lowest(int inputNum)
+{
+    int divisor = 2;
+
+    while (inputNum % divisor != 0)
+    {
+        divisor = divisor + 1;
+    }
+
+    return divisor;
+}
+
+/* Determine if the input number is prime */
+bool isPrime(int inputNum)
+{
+    if (inputNum == lowest(inputNum))
+    {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+/* Function to find a particular prime. The variable primeCount is the 
+ * particular prime we want to find. For example, if we want the 6th
+ * prime, we iterate the loop until this variable reaches 6. Each time
+ * through the loop, the prime variable is reassigned to the next prime
+ * found and the primeCount variable is iterated by 1. Once the primeCount
+ * variable reaches the specified number, the prime from that iteration is 
+ * returned. */
+int calculatePrimes(void)
+{
+    int i = 0;
+    int prime;
+    int primeCount = 0;
+
+    while (primeCount != 10001)
+    {
+        if (isPrime(i) == true)
+        {
+            prime = i;
+            primeCount++;
+        }
+        
+        i++;
+    }
+    
+    return prime;
+
+}
