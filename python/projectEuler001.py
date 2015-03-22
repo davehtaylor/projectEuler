c63987f8b121ccddaaaec0474ad3830cbc152897
Dave Taylor <davehtaylor@me.com>
2015-03-22 00:05:36 -0400

    Moved program logic into a function

--- python/projectEuler001.py
+++ python/projectEuler001.py
@@ -1,9 +1,13 @@
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-threeAndFive = 0
+def threeAndFive():
+    """Find the sum of all the multiples of 3 or 5 below 1000."""
+    total = 0
+    
+    for i in xrange(1,1000):
+        if i%3 == 0  or i%5 == 0:
+            total = total + i
 
-for i in xrange(1,1000):
-    if i%3 == 0  or i%5 == 0:
-        threeAndFive = threeAndFive + i
+    return total
 
-print threeAndFive
+print threeAndFive()
