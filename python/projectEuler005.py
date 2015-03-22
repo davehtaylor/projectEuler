7984a0f64bc9cc68961582c1369fc22823420144
Dave Taylor <davehtaylor@me.com>
2015-03-22 00:17:38 -0400

    Moved program logic into a function

--- python/projectEuler005.py
+++ python/projectEuler005.py
@@ -1,18 +1,25 @@
 # What is the smallest positive number that is evenly divisible
 # by all of the numbers from 1 to 20?
 
-i = 2520
+def divide():
+    """Find the number that is the smallest positive number 
+    that is evenly divisible by all of the numbers from 1 to 20
+    """
+    # Start at 2520, since the number won't be smaller than this
+    i = 2520
 
-while ((i%11 == 0) and \
-        (i%12 == 0) and \
-        (i%13 == 0) and \
-        (i%14 == 0) and \
-        (i%15 == 0) and \
-        (i%16 == 0) and \
-        (i%17 == 0) and \
-        (i%18 == 0) and \
-        (i%19 == 0) and \
-        (i%20 == 0)) == False:
-    i += 1
+    while ((i%11 == 0) and \
+            (i%12 == 0) and \
+            (i%13 == 0) and \
+            (i%14 == 0) and \
+            (i%15 == 0) and \
+            (i%16 == 0) and \
+            (i%17 == 0) and \
+            (i%18 == 0) and \
+            (i%19 == 0) and \
+            (i%20 == 0)) == False:
+        i += 1
 
-print i
+    return i
+
+print divide()
